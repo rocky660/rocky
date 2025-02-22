cab7969595c7a96621b287e74041ecb9da5889d4
rocky660 <bilmemki5553@gmail.com>
2025-02-22 13:24:18 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,12 +13,9 @@ class MyDocument extends Document {
             name="description"
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@MusicMaker" />
-          <meta name="twitter:creator" content="@MusicMaker" />
-          <meta property="og:url" content="https://musicmaker.vercel.app" />
+          <meta property="og:url" content="https://rocky.me.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="MusicMaker" />
+          <meta property="og:title" content="Rocky.me" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -29,11 +26,11 @@ class MyDocument extends Document {
             content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
           />
           <meta property="og:image" content="/img/logo.jpg" />
-          <meta property="og:image:alt" content="MusicMaker" />
+          <meta property="og:image:alt" content="Rocky.me" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="MusicMaker"
+            content="Rocky.me"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
