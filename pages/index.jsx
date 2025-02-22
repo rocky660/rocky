2ef6292ca3380f78e5e5e3f1c36ac8c268067f3b
rocky660 <bilmemki5553@gmail.com>
2025-02-22 13:40:45 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Index() {
           Rocky.me
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with MusicMaker? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
+       How about moderating your own server with Rocky.me? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
